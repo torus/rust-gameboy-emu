9891d83e6481bd16bc0751bd8d09c619f398f235
Toru Hisai <toru@torus.jp>
2025-01-13 17:03:49 +0900

    nop and ld

--- src/instructions.rs
+++ src/instructions.rs
@@ -1,9 +1,3 @@
-impl Cpu {
-    pub fn nop(&mut self, bus: &Peripherals) {
-        self.fetch(bus);
-    }
-}
-
 macro_rules! step {
     ($d:expr, {$($c:tt : $e:expr,)*}) => {
         static STEP: AtomicU8 = AtomicU8::new(0);
@@ -21,3 +15,41 @@ macro_rules! go {
     }
 }
 pub(crate) use go;
+
+impl Cpu {
+    pub fn nop(&mut self, bus: &Peripherals) {
+        self.fetch(bus);
+    }
+    pub fn ld<D: Copy, S: Copy>(&mut self, bus: &mut Peripherals, dst: D, src: S)
+    where Self: IO8<D> + IO8<S> {
+        step!((), {
+            0: if let Some(v) = self.read8(bus, src) {
+                VAL8.store(v, Relaxed);
+                go!(1);
+            },
+            1: if self.write8(bus, dst, VAL8.load(Relaxed)).is_some() {
+                go!(2);
+            },
+            2: {
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
+    pub fn ld16<D: Copy, S: Copy>(&mut self, bus: &mut Peripherals, dst: D, src: S)
+    where Self: IO16<D> + IO16<S> {
+        step!((), {
+            0: if let Some(v) = self.read16(bus, src) {
+                VAL16.store(v, Relaxed);
+                go!(1);
+            },
+            1: if self.write16(bus, dst, VAL16.load(Relaxed)).is_some() {
+                go!(2);
+            },
+            2: {
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
+}
