040ed18e8b34e432316b5bb8f114fecf89f41f6d
Toru Hisai <toru@torus.jp>
2025-01-13 10:54:36 +0900

    initial commit

---
+++ bootrom.rs
@@ -0,0 +1,14 @@
+pub struct Bootrom {
+    rom: Box<[u8]>,
+}
+impl Bootrom {
+    pub fn new(rom: Box<[u8]>) -> Self {
+	Self{
+	    rom,
+	}
+    }
+    pub fn read(&self, addr: u16) -> u8 {
+	self.rom[addr as usize]
+    }
+}
+
